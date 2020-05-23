2377fc4e6c0798981bab9f7cd1b403ab394947d0
ajdafrankovic <ajda.frankovic@gmail.com>
2020-05-23 16:23:44 +0200

    Add funniest session

--- javascript/funniest_session.js
+++ javascript/funniest_session.js
@@ -4,20 +4,25 @@ const s8 = ( sketch ) => {
     var table;
     var laughter;
     var w;
+    var seja;
+    var funny_level;
 
     sketch.preload = function() {
-        table = sketch.loadTable("parsed_data/events/list_of_all_different_events.csv");
+        table = sketch.loadTable("parsed_data/events/laughter.csv");
     }
 
     sketch.setup = function() {
-        laughter = 0;
-        for(let i = 0; i < table.getRowCount(); i++) {
-            let str = table.getString(i, 0);
-            if (str.includes("smeh") || str.includes("Smeh")) {
-                laughter++;
-            }
-        }
+        // laughter = 0;
+        // for(let i = 0; i < table.getRowCount(); i++) {
+        //     let str = table.getString(i, 0);
+        //     if (str.includes("smeh") || str.includes("Smeh")) {
+        //         laughter++;
+        //     }
+        // }
         //console.log("smeh:", laughter);
+        seja = table.get(0, 0);
+        seja = sessionNameFormating(seja);
+        funny_level = table.get(0, 1);
 
         sketch.createCanvasAndWriteTitle();
     }
@@ -33,14 +38,22 @@ const s8 = ( sketch ) => {
     sketch.createCanvasAndWriteTitle = function() {
         w = sketch.windowWidth;
         if(w > 1000) w = w/2;
-        canvas = sketch.createCanvas(w, 50);
+
+        canvas = sketch.createCanvas(w, 80);
         canvas.parent("funniest_session");
 
         sketch.textFont("Courier");
         sketch.textStyle(sketch.BOLD);
         sketch.textSize(24);
         sketch.textAlign(sketch.CENTER, sketch.CENTER);
-        //sketch.text("Seja z največ smeha", 0, 0, w, 50);
+        sketch.text("Seja z največ smeha", 0, 0, w, 30);
+
+        sketch.textStyle(sketch.NORMAL);
+        sketch.textSize(16);
+        sketch.text(seja, 0, 30, w, 20);
+
+        my_string = "SMEH: " + funny_level + "-krat";
+        sketch.text(my_string, 0, 55, w, 20);
     }
 
 };
